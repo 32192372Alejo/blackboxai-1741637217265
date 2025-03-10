d250831292936f567af6d4c70d96605e863191bd
32192372Alejo <alejo290816@gmail.com>
2025-03-10 15:08:44 -0500

    Add MainActivity.kt 1741637223541

---
+++ MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.interviewfaceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialize buttons
+        val btnIniciarSesion = findViewById<Button>(R.id.btnIniciarSesion)
+        val btnRegistrarse = findViewById<Button>(R.id.btnRegistrarse)
+
+        // Set click listeners
+        btnIniciarSesion.setOnClickListener {
+            // TODO: Implement login functionality
+            Toast.makeText(this, getString(R.string.toast_login), Toast.LENGTH_SHORT).show()
+        }
+
+        btnRegistrarse.setOnClickListener {
+            // TODO: Implement registration functionality
+            Toast.makeText(this, getString(R.string.toast_register), Toast.LENGTH_SHORT).show()
+        }
+    }
+}
